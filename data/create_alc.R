3169e820631bfd840fcfc6a8b221c918c9ac4db7
Chang <chang.rajani@gmail.com>
2021-11-19 16:01:34 +0200

    week3 done

---
+++ data/create_alc.R
@@ -0,0 +1,99 @@
+# Chang
+# 19.11
+# data from https://archive.ics.uci.edu/ml/machine-learning-databases/00320/
+
+
+por <- read.table("data/student-por.csv", sep = ";", header=TRUE)
+math <- read.table("data/student-mat.csv", sep = ";", header=TRUE)
+print(dim(math))
+print(str(por))
+
+
+# Define own id for both datasets
+library(dplyr)
+por_id <- por %>% mutate(id=1000+row_number()) 
+math_id <- math %>% mutate(id=2000+row_number())
+
+# Which columns vary in datasets
+free_cols <- c("id","failures","paid","absences","G1","G2","G3")
+
+# The rest of the columns are common identifiers used for joining the datasets
+join_cols <- setdiff(colnames(por_id),free_cols)
+
+pormath_free <- por_id %>% bind_rows(math_id) %>% select(one_of(free_cols))
+
+# Combine datasets to one long data
+#   NOTE! There are NO 382 but 370 students that belong to both datasets
+#         Original joining/merging example is erroneous!
+pormath <- por_id %>% 
+  bind_rows(math_id) %>%
+  # Aggregate data (more joining variables than in the example)  
+  group_by(.dots=join_cols) %>%  
+  # Calculating required variables from two obs  
+  summarise(                                                           
+    n=n(),
+    id.p=min(id),
+    id.m=max(id),
+    failures=round(mean(failures)),     #  Rounded mean for numerical
+    paid=first(paid),                   #    and first for chars
+    absences=round(mean(absences)),
+    G1=round(mean(G1)),
+    G2=round(mean(G2)),
+    G3=round(mean(G3))    
+  ) %>%
+  # Remove lines that do not have exactly one obs from both datasets
+  #   There must be exactly 2 observations found in order to joining be succesful
+  #   In addition, 2 obs to be joined must be 1 from por and 1 from math
+  #     (id:s differ more than max within one dataset (649 here))
+  filter(n==2, id.m-id.p>650) %>%  
+  # Join original free fields, because rounded means or first values may not be relevant
+  inner_join(pormath_free,by=c("id.p"="id"),suffix=c("",".p")) %>%
+  inner_join(pormath_free,by=c("id.m"="id"),suffix=c("",".m")) %>%
+  # Calculate other required variables  
+  ungroup %>% mutate(
+    alc_use = (Dalc + Walc) / 2,
+    high_use = alc_use > 2,
+    cid=3000+row_number()
+  )
+print(colnames(pormath))
+
+print(str(pormath))
+
+# dplyr, math_por, join_by are available
+
+# print out the column names of 'math_por'
+math_por = pormath
+join_by = join_cols
+# create a new data frame with only the joined columns
+alc <- select(math_por, one_of(c(join_cols, 'alc_use', 'high_use')))
+
+# the columns in the datasets which were not used for joining the data
+notjoined_columns <- colnames(math)[!colnames(math) %in% join_by]
+
+# print out the columns not used for joining
+
+
+# for every column name not used for joining...
+for(column_name in notjoined_columns) {
+  # select two columns from 'math_por' with the same original name
+  two_columns <- select(math_por, starts_with(column_name))
+  # select the first column vector of those two columns
+  first_column <- select(two_columns, 1)[[1]]
+  
+  # if that first column vector is numeric...
+  if(is.numeric(first_column)) {
+    # take a rounded average of each row of the two columns and
+    # add the resulting vector to the alc data frame
+    alc[column_name] <- round(rowMeans(two_columns))
+  } else { # else if it's not numeric...
+    # add the first column vector to the alc data frame
+    alc[column_name] <- first_column#"change me!"
+  }
+}
+
+# glimpse at the new combined data
+print(dim(alc))
+print(colnames(alc))
+
+write.csv(alc, 'data/alc_data.csv', row.names = FALSE)
+print(str(read.csv('data/alc_data.csv')))
